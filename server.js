5e12ed10c3504d3a9b44e692a829f032b6ed79d8
Norman Potts <storminnormanpotts@hotmail.com>
2020-07-25 15:26:35 -0400

    configured site with images and stuff

--- server.js
+++ server.js
@@ -6,14 +6,12 @@ const HTTP = require('http').Server.app;
 const PATH = require('path');
 const FS = require('fs'); 
 const APP = EXPRESS();
-const PORT = 8079;
+const PORT = 8078;
 
 const REGULARROUTES = require('./routes/regularroutes');
 const BODYPARSER = require('body-parser');
 
-const MESSAGESDATABASE = require('./models/messages/messagesdatabase.js');//// Put the database stuff in a module.
-MESSAGESDATABASE.plzdomydbsetup(); //// Run Database Set up.
-  
+
 ////Set Static Path to Public folder.  
 APP.use(EXPRESS.static(PATH.join(__dirname,'public')));
 
@@ -22,7 +20,4 @@ APP.use(BODYPARSER.json( {limit: '5mb' })); //// middleware for parsing json obj
 APP.use(BODYPARSER.urlencoded({ extended: false})); //// middleware for parsing bodies from URL
       
 APP.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-
-//// All routing goes threw REGULARROUTES file.
-APP.use('/', REGULARROUTES);
 
