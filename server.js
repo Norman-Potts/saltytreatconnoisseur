2ad310ee1eb7e923c43d7336e28a4f858ffa2449
Norman Potts <storminnormanpotts@hotmail.com>
2020-07-25 15:36:32 -0400

    $m

--- server.js
+++ server.js
@@ -9,17 +9,12 @@ const APP = EXPRESS();
 const PORT = 8078;
 
 
-
-
-const BODYPARSER = require('body-parser');
-
+ 
 
 ////Set Static Path to Public folder.  
 APP.use(EXPRESS.static(PATH.join(__dirname,'public')));
 
-APP.use(EXPRESS.json()); //// " a method inbuilt in express to recognize the incoming Request Object as a JSON Object" s   
-APP.use(BODYPARSER.json( {limit: '5mb' })); //// middleware for parsing json objects
-APP.use(BODYPARSER.urlencoded({ extended: false})); //// middleware for parsing bodies from URL
+ 
       
 APP.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
